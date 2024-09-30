15bbfa55051043baf1a1312d3f58f390d25a8fd1
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-09-30 21:56:52 +0900

    Add j_json library and initialize it in main function

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,18 @@
 const std = @import("std");
 const capy = @import("capy");
+const j_json = @import("libs/j_json.zig");
 
 // This is required for your app to build to WebAssembly and other particular architectures
+
 pub usingnamespace capy.cross_platform;
 
 pub fn main() !void {
+    try j_json.init();
+    try j_json.init_json();
+    try init_window();
+}
+
+fn init_window() !void {
     try capy.backend.init();
 
     var window = try capy.Window.init();
